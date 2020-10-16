391e023ed5d9de0389db73e015ac87becb199c8d
c0derbr1t <c0der.br1t@gmail.com>
2020-10-16 12:51:21 -0500

    Added CMP, JMP, JEQ, and JNE.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,28 +2,28 @@
 
 import sys
 
-# ADD HLT instruction definition by name
+# Add instructions definition by name
 HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
 MUL = 0b10100010
 PUSH = 0b01000101
 POP = 0b01000110
+CMP = 0b10100111
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
+
 SP = 7
 
 class CPU:
     """Main CPU class."""
-
     def __init__(self):
         """Construct a new CPU."""
-        # Add properties to hold 256 byes of memory and 8 general purpose registers. See step 1 and guided project.
-        # Add property for PC and any other internal registers needed. See internal registers from Spec.
-        # See rest of this class to find variables needed.
         self.reg = [0] * 8
         self.reg[SP] = 0xf4
         self.pc = 0
-        self.mar = 0
-        self.mdr = 0
+        self.fl = 0b00000000
         self.ram = [0] * 256
         self.branchtable = {}
         self.branchtable[HLT] = self.handle_hlt
@@ -32,14 +32,14 @@ class CPU:
         self.branchtable[MUL] = self.handle_mul
         self.branchtable[PUSH] = self.handle_push
         self.branchtable[POP] = self.handle_pop
+        self.branchtable[CMP] = self.handle_cmp
+        self.branchtable[JMP] = self.handle_jmp
+        self.branchtable[JEQ] = self.handle_jeq
+        self.branchtable[JNE] = self.handle_jne
 
 
     def load(self):
         """Load a program into memory."""
-        # print("\nARGV:")
-        # print(sys.argv[1])
-        # print("\n")
-
         address = 0
 
         if len(sys.argv) != 2:
@@ -50,14 +50,13 @@ class CPU:
             with open(sys.argv[1]) as f:
                 for line in f:
                     line = line.strip()
-                    # print(line)
+
                     if line == "" or line[0] == "#":
                         continue
 
                     try:
                         str_value = line.split("#")[0]
                         value = int(str_value, 2)
-                        
 
                     except ValueError:
                         print(f"Invalid Number: {str_value}")
@@ -70,42 +69,8 @@ class CPU:
             print(f"FileNotFound: {sys.argv[1]}")
             sys.exit(2)
 
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8  Tells that we are going to set the value of a register to an int
-        #     0b00000000, # Indicates R0
-        #     0b00001000, # Indicates the number 8 in binary
-        #     0b01000111, # PRN R0  Tells that we are going to print the value of an int stored in a register
-        #     0b00000000, # Indicates R0
-        #     0b00000001, # HLT  HALTS
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
-
-    def ram_read(self, address):
-        # Add method. See Step 2
-        # self.mar
-        self.mar = address
-        return self.ram[self.mar]
-
-    def ram_write(self, value, address):
-        # Add method. See Step 2
-        # self.mdr
-        self.mar = address
-        self.mdr = value
-        self.ram[self.mar] = self.mdr
-
-    def reg_write(self, value, address):
-        self.reg[address] = value
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
@@ -117,7 +82,6 @@ class CPU:
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
             #self.fl,
@@ -132,39 +96,41 @@ class CPU:
 
         print()
 
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
+    def reg_write(self, value, address):
+        self.reg[address] = value
+
     def print_reg(self):
         for i, item in enumerate(self.reg):
             print(f"R{i}: {item}")
         print("\n")
 
     def handle_hlt(self, a=None, b=None):
-        # x = "HLT"
         # print("run HLT")
         self.halted = True
-        # sys.exit(0)
 
     def handle_ldi(self, a, b):
-        # x = "LDI"
         # print("run LDI")
         self.reg_write(b, a)
-        # self.reg[operand_a] = operand_b
         self.pc += 3
 
     def handle_prn(self, a, b=None):
-        # x = "PRN"
         # print("run PRN")
         print(self.reg[a])
         self.pc += 2
 
     def handle_mul(self, a, b):
-        # x = "MUL"
         # print("run MUL")
         val = self.reg[a] * self.reg[b]
         self.reg_write(val, a)
         self.pc += 3
 
     def handle_push(self, a, b=None):
-        # x = "PUSH"
         # print("run PUSH")
         # decrement SP
         self.reg[SP] -= 1
@@ -178,43 +144,63 @@ class CPU:
         self.ram[top_of_stack] = value
         self.pc += 2
 
-        # print(self.ram[0xf0:0xf4])
-
     def handle_pop(self, a, b=None):
-        # x = "POP"
         # print("run POP")
         # get value from top of stack
         top_of_stack = self.reg[SP]
         value = self.ram[top_of_stack]
 
         # store in reg
-        # reg_num = self.reg[a]
         self.reg[a] = value
 
         # Increment SP
         self.reg[SP] += 1
         self.pc += 2
 
+    def handle_cmp(self, a, b):
+        # print("run CMP")
+        # print(f"({a}, {b}):")
+        if a < b:
+            self.fl = 0b00000100
+        elif a > b:
+            self.fl = 0b00000010
+        elif a == b:
+            self.fl = 0b00000001
+        self.pc += 3
+
+    def handle_jmp(self, a, b=None):
+        # print("run JMP")
+        value = self.reg[a]
+        # print(value)
+        self.pc = value
+
+    def handle_jeq(self, a, b=None):
+        # print("run JEQ")
+        if self.fl == 0b00000001:
+            self.handle_jmp(a)
+        else:
+            self.pc += 2
+
+    def handle_jne(self, a, b=None):
+        # print("run JNE")
+        if self.fl != 0b00000001:
+            self.handle_jmp(a)
+        else:
+            self.pc += 2
+
     def run(self):
-        # x = "run()"
         """Run the CPU."""
-        # Set local variable IR. Implement core of this method. See Step 3 amd Specs.
-        # Implement steps 4, 5, and 6.
         self.halted = False
         # print("run start")
-        # num = 1
 
         while not self.halted:
-            # print(num)
-            # num += 1
-            IR = self.ram[self.pc]
+            self.IR = self.ram[self.pc]
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            if IR in self.branchtable:
-                self.branchtable[IR](operand_a, operand_b)
+            if self.IR in self.branchtable:
+                self.branchtable[self.IR](operand_a, operand_b)
 
             else:
-                #unknown instruction
-                print(f"unknown instruction {IR} at address {self.pc}")
+                print(f"unknown instruction {self.IR} at address {self.pc}")
                 sys.exit(1)
